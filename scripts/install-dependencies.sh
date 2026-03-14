8585808929417daa2abb6f5bcc0371e5dd8e6a79
Your Name <you@example.com>
2026-03-14 10:19:31 +0100

    ok

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -29,109 +29,62 @@ if [ -f .nvmrc ]; then
 fi
 export NODE_OPTIONS=--max-old-space-size=12096
 
-
-# echo "Installing root-level dependencies..."
-# npm install
+echo "Installing root-level dependencies..."
+npm install
  
-# echo "Building packages (fetch, openai-adapters, config-yaml)..."
-# node ./scripts/build-packages.js
+echo "Building packages (fetch, openai-adapters, config-yaml)..."
+node ./scripts/build-packages.js
 
-# echo "Installing Core extension dependencies..."
-# pushd core
-# ## This flag is set because we pull down Chromium at runtime
-# export PUPPETEER_SKIP_DOWNLOAD='true'
-# npm install
-# npm link
-# popd
+echo "Installing Core extension dependencies..."
+pushd core
+## This flag is set because we pull down Chromium at runtime
+export PUPPETEER_SKIP_DOWNLOAD='true'
+npm install
+npm link
+popd
 
-# pushd packages/openai-adapters
-# npm install
-# npm run build         # Si besoin
-# npm link
-# cd ../../core
-# npm install
-# npm link ../packages/openai-adapters   # Si openai-adapters n'est pas publié sur npm
-# npm link                 # Pour rendre core globalement linkable
-# popd
+pushd packages/openai-adapters
+npm install
+npm run build         # Si besoin
+npm link
+cd ../../core
+npm install
+npm link ../packages/openai-adapters   # Si openai-adapters n'est pas publié sur npm
+npm link                 # Pour rendre core globalement linkable
+popd
 
-# echo "Installing GUI extension dependencies..."
-# pushd gui
-# npm install
-# # npm link @continuedev/core
+echo "Installing GUI extension dependencies..."
+pushd gui
+npm install
 # npm link @continuedev/core
-# NODE_OPTIONS="--max-old-space-size=4096" npm run build
-# popd
-
-# # VSCode Extension (will also package GUI)
-# echo "Installing VSCode extension dependencies..."
-# pushd extensions/vscode
-# # This does way too many things inline but is the common denominator between many of the scripts
-# npm install
-# # npm link @continuedev/core
-# # npm run prepackage # not required since npm run package has prescript of prepackage
-# npm run package
-# popd
-
-# echo "Installing binary dependencies..."
-# pushd binary
-# npm install
-# npm run build
-# popd
-
-# echo "Installing root-level dependencies..."
-# npm install
-
-# echo "Building packages... (fetch, openai-adapters, config-yaml)..."
-# node ./scripts/build-packages.js
-
-# echo "Building config-types..."
-# pushd packages/config-types
-# npm install
-# npm run build
-# popd
-
-# echo "Installing root-level dependencies..."
-# npm install
-
-# echo "Building packages (fetch, openai-adapters, config-yaml)..."
-# node ./scripts/build-packages.js
-
-# echo "Installing Core extension dependencies..."
-# pushd core
-# ## This flag is set because we pull down Chromium at runtime
-# export PUPPETEER_SKIP_DOWNLOAD='true'
-# npm install
-# npm link
-# popd
-
-# echo "Installing GUI extension dependencies..."
-# pushd gui
-# npm install
-# npm link @continuedev/core
-# NODE_OPTIONS="--max-old-space-size=12096" npm run build
-# popd
+npm link @continuedev/core
+NODE_OPTIONS="--max-old-space-size=4096" npm run build
+popd
 
 # VSCode Extension (will also package GUI)
 echo "Installing VSCode extension dependencies..."
 pushd extensions/vscode
-# # This does way too many things inline but is the common denominator between many of the scripts
+# This does way too many things inline but is the common denominator between many of the scripts
 npm install
-npm link @continuedev/core
+# npm link @continuedev/core
 # npm run prepackage # not required since npm run package has prescript of prepackage
-NODE_OPTIONS="--max-old-space-size=12096"  npm run package-all
-# npm run package-all
+npm run package
 popd
 
-# echo "Installing binary dependencies..."
-# pushd binary
-# npm install
-# npm run build
-# popd
+echo "Installing binary dependencies..."
+pushd binary
+npm install
+npm run build
+popd
 
-# echo "Installing docs dependencies..."
-# pushd docs
-# npm install
-# popd
+echo "Installing docs dependencies..."
+pushd docs
+npm install
+popd
+
+
+
+########################### Others
 
 # pushd extensions/vscode
 # echo "Clear dependencies Vscode"
